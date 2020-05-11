5ea1f12ba1a43e354f462aea3f54d2adab1d3e5e
elainewo <yw.4contact@gmail.com>
2020-05-11 16:13:51 +0200

    update

--- src/Components/MovieItem.js
+++ src/Components/MovieItem.js
@@ -1,28 +1,25 @@
-import React from 'react';
-import './movieitem.css'
-
-const stylePgenre = {
-    lineHeight: ".5rem",
-}
-
-const styleP = {
-    lineHeight: "1.2rem",
-    fontWeight: "bold"
-}
+import React from "react";
+import "./movieitem.css";
 
 const MovieItem = (props) => {
-    return (
-        <div className="movie-div">
-            <h2>{props.title}</h2>
-            <p style={styleP}>{props.year}</p>
-            <p style={styleP}>{props.director}</p>
-            <p style={styleP}>{props.duration}</p>
-            <p style={styleP}>{props.rate}</p>
-            <p style={stylePgenre}>{props.genre[0]}</p>
-            <p style={stylePgenre}>{props.genre[1]}</p>
-            <p style={stylePgenre}>{props.genre[2]}</p>
-        </div>
-    );
-}
+  return (
+    <div className="movie-div">
+      <h2>{props.title}</h2>
+      <p>{props.year}</p>
+      <p>{props.director}</p>
+      <p>{props.duration}</p>
+      <p>{props.rate} &#9734;</p>
+      <ul>
+        {props.genre.map((genre, i) => {
+          return (
+            <li key={i}>
+              <div className="border">{genre}</div>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
 
 export default MovieItem;
--- src/Components/MovieList.js
+++ src/Components/MovieList.js
@@ -1,9 +1,8 @@
 // import React from 'react';
-import React, { Component } from 'react';
-import movies from './MovieData';
-import MovieItem from './MovieItem';
-import './movielist.css';
-
+import React, { Component } from "react";
+import movies from "./MovieData";
+import MovieItem from "./MovieItem";
+import "./movielist.css";
 
 // const data = movies.map(x => <MovieItem title={x.title} year={x.year} director={x.director} duration={x.duration} rate={x.rate} genre={x.genre} key={x} />)
 // const MovieList = () => {
@@ -22,103 +21,113 @@ import './movielist.css';
 
 // export default MovieList;
 
-
 class MovieList extends Component {
-    state = {
-        movieDataState: movies
-    }
-    handleSortAZ = () => {
-        const sortedArray = this.state.movieDataState.sort((a, b) => {
-            let eltA = a.title.toUpperCase()
-            let eltB = b.title.toUpperCase()
-            if (eltA < eltB) {
-                return -1
-            } else if (eltA > eltB) {
-                return 1
-            } else {
-                return 0
-            }
-        })
-        this.setState({ movieDataState: sortedArray })
-    }
-    handleSortZA = () => {
-        const sortedArray = this.state.movieDataState.sort((a, b) => {
-            let eltA = a.title.toUpperCase()
-            let eltB = b.title.toUpperCase()
-            if (eltA < eltB) {
-                return 1
-            } else if (eltA > eltB) {
-                return -1
-            } else {
-                return 0
-            }
-        })
-        this.setState({ movieDataState: sortedArray })
-    }
-    handleSortDateAs = () => {
-        const sortedArray = this.state.movieDataState.sort((a, b) => {
-            let eltA = a.year.toUpperCase()
-            let eltB = b.year.toUpperCase()
-            if (eltA < eltB) {
-                return -1
-            } else if (eltA > eltB) {
-                return 1
-            } else {
-                return 0
-            }
-        })
-        this.setState({ movieDataState: sortedArray })
-    }
-    handleSortDateDes = () => {
-        const sortedArray = this.state.movieDataState.sort((a, b) => {
-            let eltA = a.year.toUpperCase()
-            let eltB = b.year.toUpperCase()
-            if (eltA < eltB) {
-                return 1
-            } else if (eltA > eltB) {
-                return -1
-            } else {
-                return 0
-            }
-        })
-        this.setState({ movieDataState: sortedArray })
-    }
-    handleSortRate = () => {
-        const sortedArray = this.state.movieDataState.sort((a, b) => {
-            let eltA = a.rate.toUpperCase()
-            let eltB = b.rate.toUpperCase()
-            if (eltA < eltB) {
-                return 1
-            } else if (eltA > eltB) {
-                return -1
-            } else {
-                return 0
-            }
-        })
-        this.setState({ movieDataState: sortedArray })
-    }
-    render() {
-        const data = this.state.movieDataState.map((x, i) =>
-            <MovieItem title={x.title}
-                year={x.year}
-                director={x.director}
-                duration={x.duration}
-                rate={x.rate}
-                genre={x.genre}
-                key={i} />)
-        return (
-            <main>
-                <button onClick={this.handleSortDateAs}> Sort by Date Ascending</button>
-                <button onClick={this.handleSortDateDes}>Sort by Date Descending</button>
-                <button onClick={this.handleSortRate}>Best Rate</button>
-                <button onClick={this.handleSortAZ}>A-Z</button>
-                <button onClick={this.handleSortZA}>Z-A</button>
-                <section className="movie-style">
-                    {data}
-                </section>
-            </main>
-        );
-    }
+  state = {
+    movieDataState: movies,
+  };
+  handleSortAZ = () => {
+    const sortedArray = this.state.movieDataState.sort((a, b) => {
+      let eltA = a.title.toUpperCase();
+      let eltB = b.title.toUpperCase();
+      if (eltA < eltB) {
+        return -1;
+      } else if (eltA > eltB) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+    this.setState({ movieDataState: sortedArray });
+  };
+  handleSortZA = () => {
+    const sortedArray = this.state.movieDataState.sort((a, b) => {
+      let eltA = a.title.toUpperCase();
+      let eltB = b.title.toUpperCase();
+      if (eltA < eltB) {
+        return 1;
+      } else if (eltA > eltB) {
+        return -1;
+      } else {
+        return 0;
+      }
+    });
+    this.setState({ movieDataState: sortedArray });
+  };
+  handleSortDateAs = () => {
+    const sortedArray = this.state.movieDataState.sort((a, b) => {
+      let eltA = a.year.toUpperCase();
+      let eltB = b.year.toUpperCase();
+      if (eltA < eltB) {
+        return -1;
+      } else if (eltA > eltB) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+    this.setState({ movieDataState: sortedArray });
+  };
+  handleSortDateDes = () => {
+    const sortedArray = this.state.movieDataState.sort((a, b) => {
+      let eltA = a.year.toUpperCase();
+      let eltB = b.year.toUpperCase();
+      if (eltA < eltB) {
+        return 1;
+      } else if (eltA > eltB) {
+        return -1;
+      } else {
+        return 0;
+      }
+    });
+    this.setState({ movieDataState: sortedArray });
+  };
+  handleSortRate = () => {
+    const sortedArray = this.state.movieDataState.sort((a, b) => {
+      let eltA = a.rate.toUpperCase();
+      let eltB = b.rate.toUpperCase();
+      if (eltA < eltB) {
+        return 1;
+      } else if (eltA > eltB) {
+        return -1;
+      } else {
+        return 0;
+      }
+    });
+    this.setState({ movieDataState: sortedArray });
+  };
+  sortByAction = () => {
+    const filter = this.state.movieDataState.filter((movie) => {
+      // return movie.genre.indexOf("Action") !== -1
+      return movie.genre.includes("Action");
+    });
+    this.setState({ movieDataState: filter });
+  };
+  render() {
+    const data = this.state.movieDataState.map((x, i) => (
+      <MovieItem
+        title={x.title}
+        year={x.year}
+        director={x.director}
+        duration={x.duration}
+        rate={x.rate}
+        genre={x.genre}
+        key={i}
+      />
+    ));
+    return (
+      <main>
+        <button onClick={this.handleSortDateAs}> Sort by Date Ascending</button>
+        <button onClick={this.handleSortDateDes}>
+          Sort by Date Descending
+        </button>
+        <button onClick={this.handleSortRate}>Best Rate</button>
+        <button onClick={this.handleSortAZ}>A-Z</button>
+        <button onClick={this.handleSortZA}>Z-A</button>
+        <button onClick={this.sortByAction}>Filter: include "Action"</button>
+        <section className="movie-style">{data}</section>
+      </main>
+    );
+  }
 }
 
 export default MovieList;
